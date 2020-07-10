76c6c8692c9623b3c6b35d03964203026aceb896
David Temple <dstemple7@gmail.com>
2020-07-10 10:29:42 -0500

    plants rendering

--- client/src/App.js
+++ client/src/App.js
@@ -31,7 +31,7 @@ function App() {
           <ul className="steps">
             <li>
               <NavLink exact to="/">
-                Plants, etc.
+                Plants
               </NavLink>
             </li>
             <li>
